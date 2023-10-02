079f3115154ddbbdaee0277a41dc12ce790583ae
t22007997 <elfamosorayto@gmail.com>
2023-10-02 09:48:34 +0200

    Change index page

--- modules/rtff/views/ConnexionPage.php
+++ modules/rtff/views/ConnexionPage.php
@@ -6,16 +6,16 @@ class ConnexionPage
     public function show(): void
     {
         ob_start();
-?><div style="margin-left:220px; padding:10px;">
-    <!-- Formulaire de connexion -->
-    <form method="post" action="connectUser.php">
-        Email: <input type="email" name="account_id" required><br>
-        Mot de Passe: <input type="password" name="password" required><br>
-        <input type="submit" value="Se connecter">
-    </form>
-    <a href="requestMailForReset.php">Mot de passe oublié</a><br>
-    <a href="createUser.php">Créer un compte</a>
-</div>
+?>
+<!-- Formulaire de connexion -->
+<form method="post" action="connectUser.php">
+    Email: <input type="email" name="account_id" required><br>
+    Mot de Passe: <input type="password" name="password" required><br>
+    <input type="submit" value="Se connecter">
+</form>
+<a href="requestMailForReset.php">Mot de passe oublié</a><br>
+<a href="createUser.php">Créer un compte</a>
+
 <?php
         (new \rtff\views\Layout('Login', ob_get_clean()))->show();
     }
