e98a0d2ffa37367e6c4a025bbd1e3dc2f79f52ee
t22007997 <elfamosorayto@gmail.com>
2023-10-02 10:07:28 +0200

    Amelioration and correction user

--- modules/rtff/controllers/authentication/CreateUser.php
+++ modules/rtff/controllers/authentication/CreateUser.php
@@ -4,12 +4,14 @@ use rtff\models\User;
 
 require_once '../../models/User.php';
 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user_id = $_POST['user_id'];
     $password = $_POST['password'];
-    $notification = User::connectUser($user_id, $password);
+    $display_name = $_POST['display_name'];
+
+    $notification = User::createUser($user_id, $password, $display_name); // Supposition d'une méthode statique
     echo $notification;
 }
 
 require_once '../../views/CreateUserPage.php';
-
--- modules/rtff/views/ConnexionPage.php
+++ modules/rtff/views/ConnexionPage.php
@@ -6,23 +6,24 @@ class ConnexionPage
     public function show(): void
     {
         ob_start();
-?><div>
-    <!-- Formulaire de connexion -->
-    <form method="post" action="connectUser.php">
-        <label for="account_id"> Email<br>
-            <input type="email" name="account_id" required>
-        </label><br>
+        ?>
+        <div>
+            <!-- Formulaire de connexion -->
+            <form method="post" action="ConnectUserController.php"> <!-- Modifié ici -->
+                <label for="account_id"> Email<br>
+                    <input type="email" name="account_id" required>
+                </label><br>
 
-        <label for="password"> Mot de Passe<br>
-            <input type="password" name="password" required><br>
-        </label><br>
+                <label for="password"> Mot de Passe<br>
+                    <input type="password" name="password" required><br>
+                </label><br>
 
-        <input type="submit" value="Se connecter"><br>
-    </form><br>
-    <a href="requestMailForReset.php">Mot de passe oublié</a><br>
-    <a href="createUser.php">Créer un compte</a>
-</div>
-<?php
+                <input type="submit" value="Se connecter"><br>
+            </form><br>
+            <a href="requestMailForReset.php">Mot de passe oublié</a><br>
+            <a href="createUser.php">Créer un compte</a>
+        </div>
+        <?php
         (new \rtff\views\Layout('Login', ob_get_clean()))->show();
     }
 }
