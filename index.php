c197b01cbc97df90cc3b52f56765c049fd1bee0f
t22007997 <elfamosorayto@gmail.com>
2023-10-02 11:19:53 +0200

    Routeur

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php
+// index.php
 require_once './modules/rtff/Autoloader.php';
-rtff\Autoloader::register();
+\rtff\Autoloader::register();
 
-try {
-    (new rtff\views\ConnexionPage())->show();
-} catch (Exception $e) {
+$controller = $_GET['controller'] ?? 'defaultController';
+$action = $_GET['action'] ?? 'defaultAction';
 
+$controllerClassName = "\\rtff\\controllers\\" . ucfirst($controller) . "Controller";
+if(class_exists($controllerClassName)) {
+    $controllerObject = new $controllerClassName();
+    if(method_exists($controllerObject, $action)) {
+        $controllerObject->$action();
+    } else {
+        http_response_code(404);
+        echo "404 Not Found - Action non trouvée";
+    }
+} else {
+    http_response_code(404);
+    echo "404 Not Found - Contrôleur non trouvé";
 }
-
