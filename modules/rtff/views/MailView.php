84e95c6bf85fc08d3f74eabeebf4ee552d858c1f
t22007997 <elfamosorayto@gmail.com>
2023-10-06 09:07:36 +0200

    Mail (reset password)

--- modules/rtff/views/MailView.php
+++ modules/rtff/views/MailView.php
@@ -15,7 +15,7 @@ class MailView {
         </head>
         <body>
         <div style="margin-left:220px; padding:10px;">
-            <form method="post" action="index.php?action=../pages/MailController/sendMail">
+            <form method="post" action="../pages/MailController/sendMail">
                 Mail : <input name="account_id" type="email"/>
                 <input name="send" type="submit"/>
             </form>
