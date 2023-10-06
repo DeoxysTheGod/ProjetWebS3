fae2be3d4d0b04253b70df23d14b13dba9bf23a9
DeoxysTheGod <quentinb2004@gmail.com>
2023-10-06 08:21:01 +0200

    réparation de Navbar

---
+++ modules/rtff/views/Navbar.php
@@ -0,0 +1,29 @@
+<?php
+namespace rtff\views;
+
+class Navbar
+{
+    public function show(): void
+    {
+        ?>
+        <link rel="stylesheet" href="/assets/styles/navbar.css">
+        <nav>
+            <a href="/">
+                <div id="logo">
+                    <img src="../../../assets/images/logo-test.png" alt="logo">
+                </div id="logo">
+            </a>
+
+            <div id="title">
+                Read The F****** Forum
+            </div>
+
+            <div id="options">
+                <button>Sign in</button>
+                <button>Sign up</button>
+            </div>
+        </nav>
+        <?php
+        (new \rtff\views\Layout('Navigation', ob_get_clean()))->show();
+    }
+}
