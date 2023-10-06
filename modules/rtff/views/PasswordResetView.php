34beb9e48147aeb1823950be68d31beb45091ae5
ClementMARIEM22004869 <clement.marie24@gmail.com>
2023-10-06 09:46:44 +0200

    modifie mdp

--- modules/rtff/views/PasswordResetView.php
+++ modules/rtff/views/PasswordResetView.php
@@ -1,9 +1,15 @@
 <?php
-// Code HTML pour afficher le message et le formulaire
-if ($message != '') {
-    echo "<p>$message</p>";
-    if ($message === "Mot de passe modifié avec succès !") {
-        echo '<p><html>
+namespace rtff\views;
+
+class PasswordResetView
+{
+    public static function render($message, $token)
+    {
+        // Code HTML pour afficher le message et le formulaire
+        if ($message != '') {
+            echo "<p>$message</p>";
+            if ($message === "Mot de passe modifié avec succès !") {
+                echo '<p><html>
 <head>
     <title>Connectez-vous</title>
 </head>
@@ -12,13 +18,15 @@ if ($message != '') {
 <a href="https://rtff.alwaysdata.net/connectUser.php">Cliquez ici</a>
 </body>
 </html></p>';
-    }
-}
+            }
+        }
 
-if (isset($token) && $message == '') {
-    echo '<form method="post" action="modifyPasswordUser.php?token=' . $token . '">
-        Nouveau Mot de Passe: <input type="password" name="new_password" required><br>
-        <input type="submit" value="Modifier Mot de Passe">
-    </form>';
+        if (isset($token) && $message == '') {
+            echo '<form method="post" action="modifyPasswordUser.php?token=' . $token . '">
+                Nouveau Mot de Passe: <input type="password" name="new_password" required><br>
+                <input type="submit" value="Modifier Mot de Passe">
+            </form>';
+        }
+    }
 }
 ?>
