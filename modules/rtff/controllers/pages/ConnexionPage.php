c0e8ee2f0d99cbe75ca5d18285a2c0e55e20a363
MARIE Clément <clement.marie24@gmail.com>
2023-11-05 22:13:04 +0100

    les commentaires

--- modules/rtff/controllers/pages/ConnexionPage.php
+++ modules/rtff/controllers/pages/ConnexionPage.php
@@ -7,6 +7,11 @@ use Includes\Database\DatabaseConnexion;
 
 class ConnexionPage
 {
+    /**
+     * Exécute l'action par défaut du contrôleur.
+     *
+     * Cette méthode affiche la page de connexion en utilisant la vue appropriée.
+     */
     public function execute(): void
     {
         (new \rtff\views\ConnexionPage())->show();
