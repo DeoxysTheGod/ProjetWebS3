0ae5cbe8f569f9eaa9e2470c5507a9a03aba3ae5
JulienTORREST22007997 <elfamosorayto@gmail.com>
2023-11-05 21:53:36 +0100

    Model Documentation

--- modules/rtff/models/CategoryModel.php
+++ modules/rtff/models/CategoryModel.php
@@ -4,34 +4,58 @@ namespace rtff\models;
 
 use PDO;
 
+/**
+ * Model class responsible for handling operations related to categories.
+ */
 class CategoryModel {
-    private $db;
+    private PDO $db;
 
-    public function __construct($db) {
+    /**
+     * Constructs the CategoryModel with a database connection.
+     *
+     * @param PDO $db The database connection.
+     */
+    public function __construct(PDO $db) {
         $this->db = $db;
     }
 
-    public function getAllCategories() {
+    /**
+     * Retrieves all categories from the database.
+     *
+     * @return array An associative array of all categories.
+     */
+    public function getAllCategories(): array {
         $query = "SELECT * FROM CATEGORY";
         $stmt = $this->db->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-    public function getCategoriesSortedByUsage() {
+
+    /**
+     * Retrieves categories sorted by usage from the database.
+     *
+     * @return array An associative array of categories sorted by usage.
+     */
+    public function getCategoriesSortedByUsage(): array {
         $query = "
             SELECT CATEGORY.title, COUNT(TICKET_CATEGORIES.category_id) AS nb_usage
-			FROM CATEGORY
-			LEFT JOIN TICKET_CATEGORIES ON CATEGORY.category_id = TICKET_CATEGORIES.category_id
-			GROUP BY CATEGORY.title
-			ORDER BY nb_usage DESC";
+            FROM CATEGORY
+            LEFT JOIN TICKET_CATEGORIES ON CATEGORY.category_id = TICKET_CATEGORIES.category_id
+            GROUP BY CATEGORY.title
+            ORDER BY nb_usage DESC";
 
         $stmt = $this->db->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function addCategory($title, $description) {
-        // Ajoute une nouvelle catégorie
+    /**
+     * Adds a new category to the database.
+     *
+     * @param string $title The title of the category.
+     * @param string $description The description of the category.
+     */
+    public function addCategory(string $title, string $description): void {
         $query = "INSERT INTO CATEGORY (title, description) VALUES (:title, :description)";
         $stmt = $this->db->prepare($query);
         $stmt->bindParam(':title', $title);
@@ -39,8 +63,12 @@ class CategoryModel {
         $stmt->execute();
     }
 
-    public function deleteCategory($categoryId) {
-        // Supprime une catégorie
+    /**
+     * Deletes a category from the database.
+     *
+     * @param int $categoryId The ID of the category to be deleted.
+     */
+    public function deleteCategory(int $categoryId): void {
         $query = "DELETE FROM CATEGORY WHERE category_id = :category_id";
         $stmt = $this->db->prepare($query);
         $stmt->bindParam(':category_id', $categoryId);
