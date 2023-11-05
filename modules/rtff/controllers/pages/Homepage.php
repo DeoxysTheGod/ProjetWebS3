ed957ae4db85027470d72e38d4a440038b63f2f0
MARIE Clément <clement.marie24@gmail.com>
2023-11-05 22:16:12 +0100

    les commentaires

--- modules/rtff/controllers/pages/Homepage.php
+++ modules/rtff/controllers/pages/Homepage.php
@@ -9,16 +9,29 @@ require_once 'modules/rtff/views/Homepage.php';
 
 class Homepage
 {
+    /**
+     * @var TicketModel Le modèle pour les tickets.
+     */
     private TicketModel $ticketModel;
+    /**
+     * @var CategoryModel Le modèle pour les catégories.
+     */
 	private CategoryModel $categoryModel;
-
+    /**
+     * Constructeur de la classe.
+     */
     public function __construct() {
         $database = DatabaseConnexion::getInstance();
         $db = $database->getConnection();
         $this->ticketModel = new TicketModel($db);
 		$this->categoryModel = new CategoryModel($db);
     }
-
+    /**
+     * Affiche la page d'accueil.
+     *
+     * Cette méthode récupère les derniers cinq tickets et les catégories triées par utilisation,
+     * puis affiche la page d'accueil en utilisant la vue associée.
+     */
     public function defaultMethod() {
         $tickets = $this->ticketModel->getLastFiveTickets();
 		$categories = $this->categoryModel->getCategoriesSortedByUsage();
