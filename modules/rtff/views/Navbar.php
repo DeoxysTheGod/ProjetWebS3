0bd9c14f15736427e80e585df52a5899153a7d32
JulienTORREST22007997 <elfamosorayto@gmail.com>
2023-11-05 21:07:00 +0100

    Navbar documentation and refactor

--- modules/rtff/views/Navbar.php
+++ modules/rtff/views/Navbar.php
@@ -3,40 +3,49 @@ namespace rtff\views;
 
 class Navbar
 {
-    private function displayConnected(): void
-	{
+    /**
+     * Displays appropriate user options based on login status.
+     */
+    private function displayConnected(): void {
         if (isset($_SESSION['account_id'])) {
-            // Si l'utilisateur est connecté, affichez le bouton de déconnexion
-            ?><button class="classic-button" onclick="location.href='/authentication/logout'">Déconnexion</button>
-            <?php
+            // User is logged in: Display the logout button
+            echo '<button class="classic-button" onclick="location.href=\'/authentication/logout\'">Logout</button>';
         } else {
-            // Si l'utilisateur n'est pas connecté, affichez le bouton de connexion
-            ?>
-            <button class="classic-button" onclick="location.href='/authentication'">Connexion</button>
-            <?php
+            // User is not logged in: Display the login button
+            echo '<button class="classic-button" onclick="location.href=\'/authentication\'">Login</button>';
         }
     }
 
-    public function show(): void
-    {
+    /**
+     * Displays the navigation bar with user options based on the session's login status.
+     */
+    public function show(): void {
         ?>
+        <!-- Link to the stylesheet for the navigation bar -->
         <link rel="stylesheet" href="/assets/styles/navbar.css">
+
+        <!-- Navigation bar structure -->
         <nav class="navbar">
+            <!-- Left element: Logo -->
             <div class="navbar-element navbar-left-element">
                 <a href="/">
-                    <img id="logo" src="../../../assets/images/logo.png" alt="logo">
+                    <img id="logo" src="/assets/images/logo.png" alt="logo">
                 </a>
             </div>
 
-
+            <!-- Center element: Title -->
             <div class="navbar-element navbar-center-element" id="title">
                 Read The F****** Forum
             </div>
+
+            <!-- Right element: User options -->
             <div class="navbar-element navbar-right-element" id="options">
-                <?= $this->displayConnected(); ?>
+                <?php $this->displayConnected(); ?>
             </div>
         </nav>
         <?php
     }
+
+
 }
 
