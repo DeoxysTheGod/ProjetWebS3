ee43585f8fc9647a0e2fa286e71278ea18dc484f
MARIE Clément <clement.marie24@gmail.com>
2023-11-05 22:29:10 +0100

    les commentaires

--- modules/rtff/controllers/HomepageController.php
+++ modules/rtff/controllers/HomepageController.php
@@ -3,6 +3,12 @@ namespace rtff\controllers;
 
 class HomepageController
 {
+    /**
+     * Exécute l'action par défaut du contrôleur.
+     *
+     * Cette méthode est responsable de l'exécution de l'action par défaut du contrôleur,
+     * qui consiste à afficher la page d'accueil.
+     */
 	function execute(): void
 	{
 		(new \rtff\views\Homepage())->show();
--- modules/rtff/models/PasswordResetModel.php
+++ modules/rtff/models/PasswordResetModel.php
@@ -5,13 +5,25 @@ use rtff\database\DatabaseConnexion;
 use PDO;
 
 class PasswordResetModel {
+    /** @var PDO Connexion à la base de données. */
     private $db;
-
+    /**
+     * Constructeur de la classe.
+     *
+     * Initialise la connexion à la base de données.
+     */
     public function __construct() {
         $database = \rtff\database\DatabaseConnexion::getInstance();
         $this->db = $database->getConnection();
     }
 
+    /**
+     * Vérifie si un token est valide.
+     *
+     * @param string $token Le token à vérifier.
+     *
+     * @return bool Retourne true si le token est valide, sinon false.
+     */
     public function isValidToken($token) {
         $query = "SELECT * FROM TOKEN WHERE token_id = :token_id AND date_creation >= NOW() - INTERVAL 30 MINUTE";
         $stmt = $this->db->prepare($query);
@@ -20,7 +32,14 @@ class PasswordResetModel {
 
         return $stmt->rowCount() > 0;
     }
-
+    /**
+     * Réinitialise le mot de passe d'un utilisateur.
+     *
+     * @param string $token Le token de réinitialisation.
+     * @param string $newPassword Le nouveau mot de passe à définir.
+     *
+     * @return bool Retourne true si la réinitialisation du mot de passe a réussi, sinon false.
+     */
     public function resetPassword($token, $newPassword) {
         $new_password = password_hash($newPassword, PASSWORD_BCRYPT);
 
@@ -36,7 +55,11 @@ class PasswordResetModel {
             return false;
         }
     }
-
+    /**
+     * Supprime un token de la base de données.
+     *
+     * @param string $token Le token à supprimer.
+     */
     public function deleteToken($token) {
         $query = "DELETE FROM TOKEN WHERE token_id = :token_id";
         $stmt = $this->db->prepare($query);
