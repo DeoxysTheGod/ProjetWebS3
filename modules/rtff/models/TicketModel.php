9be9c3935a50fd6e0e5d6325b84297e2b4294934
JulienTORREST22007997 <elfamosorayto@gmail.com>
2023-11-05 12:16:04 +0100

    Add MVC Model to view all posts

--- modules/rtff/models/TicketModel.php
+++ modules/rtff/models/TicketModel.php
@@ -13,6 +13,12 @@ class TicketModel {
     public function __construct($db) {
         $this->db = $db;
     }
+    public function getAllTickets() {
+        $query = "SELECT t.*, a.display_name AS username FROM TICKET t LEFT JOIN ACCOUNT a ON t.author = a.account_id";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 
     public function getTicket($ticket_id) {
         $query = "SELECT t.*, a.display_name AS username FROM TICKET t LEFT JOIN ACCOUNT a ON t.author = a.account_id WHERE t.ticket_id = :ticket_id";
