161e9cd0a3a8b4427713e20a9f3794e85654b874
JulienTORREST22007997 <elfamosorayto@gmail.com>
2023-11-05 21:25:20 +0100

    Layout documentation

--- modules/rtff/views/Layout.php
+++ modules/rtff/views/Layout.php
@@ -1,32 +1,50 @@
 <?php
 
-    namespace rtff\views;
+namespace rtff\views;
+
+/**
+ * Layout Class
+ *
+ * Provides methods to render the main layout of the application.
+ */
 class Layout
 {
     private Navbar $navbar;
+
+    /**
+     * Constructor initializes the layout with a title and content.
+     *
+     * @param string $title   The title of the page.
+     * @param string $content The content of the page.
+     */
     public function __construct(private string $title, private string $content) {
         $this->navbar = new Navbar();
     }
+
+    /**
+     * Renders the layout of the page.
+     */
     public function show(): void
     {
-?><!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="utf-8"/>
-    <title><?= $this->title; ?></title>
-    <link rel="icon" type="image/x-icon" href="/assets/images/icons/favicon-white/favicon.ico">
-    <link rel="stylesheet" href="/assets/styles/style.css"/>
-    <link rel="stylesheet" href="/assets/styles/navbar.css">
-</head>
-<body>
-<header>
-	<?= $this->navbar->show() ?>
-</header>
-<div id="content-page">
-	<?= $this->content; ?>
-</div>
-</body>
-</html>
-<?php
+        ?>
+        <!DOCTYPE html>
+        <html lang="fr">
+        <head>
+            <meta charset="utf-8"/>
+            <title><?= $this->title; ?></title>
+            <link rel="icon" type="image/x-icon" href="/assets/images/icons/favicon-white/favicon.ico">
+            <link rel="stylesheet" href="/assets/styles/style.css"/>
+            <link rel="stylesheet" href="/assets/styles/navbar.css">
+        </head>
+        <body>
+        <header>
+            <?php $this->navbar->show(); ?>
+        </header>
+        <div id="content-page">
+            <?= $this->content; ?>
+        </div>
+        </body>
+        </html>
+        <?php
     }
 }
