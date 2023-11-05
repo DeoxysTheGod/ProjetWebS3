91b425a32f09b10e58687ed2ff95493b946dc1c2
MARIE Clément <clement.marie24@gmail.com>
2023-11-05 22:20:12 +0100

    les commentaires

--- modules/rtff/controllers/pages/Layout.php
+++ modules/rtff/controllers/pages/Layout.php
@@ -6,6 +6,11 @@ require_once 'modules/rtff/views/Layout.php';
 
 class Layout
 {
+    /**
+     * Exécute l'action par défaut du contrôleur.
+     *
+     * Cette méthode affiche le layout principal du site (avec le titre "RT*F") sans contenu spécifique.
+     */
     public function execute(): void
     {
         (new \rtff\views\Layout('RT*F', null))->show();
