7f79610ce90e4d4b4974a0f485594bf6f32c40fa
JulienTORREST22007997 <elfamosorayto@gmail.com>
2023-11-05 16:58:49 +0100

    Creation Admin Back office

--- modules/rtff/views/AdminView.php
+++ modules/rtff/views/AdminView.php
@@ -11,8 +11,8 @@ class AdminView {
         echo "<ul>";
         foreach ($posts as $post) {
             echo "<li>";
-            echo "<strong>" . htmlspecialchars($post['title'] ?? '') . "</strong>: " . htmlspecialchars($post['message'] ?? '');
             echo "<strong>" . html_entity_decode(htmlspecialchars($post['title'] ?? '')) . "</strong>: " . html_entity_decode(htmlspecialchars($post['message'] ?? ''));
+            echo " <a href='/admin/delete-post?id=" . htmlspecialchars($post['ticket_id']) . "' style='color: red;'>Supprimer</a>";
             echo "</li>";
         }
         echo "</ul>";
