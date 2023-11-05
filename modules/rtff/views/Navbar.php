9b08d372a3a2ca319a44734e4f800bae3c0c4ff6
Quentin <quentinb2004@gmail.com>
2023-11-05 18:59:38 +0100

    style de la Homepage, Navbar + fonc

--- modules/rtff/views/Navbar.php
+++ modules/rtff/views/Navbar.php
@@ -8,11 +8,12 @@ class Navbar
         ?>
         <link rel="stylesheet" href="/assets/styles/navbar.css">
         <nav class="navbar">
-            <a class="navbar-element navbar-left-element" href="/">
-                <div id="logo">
-                    <img src="../../../assets/images/logo.png" alt="logo">
-                </div id="logo">
-            </a>
+            <div class="navbar-element navbar-left-element">
+                <a href="/">
+                    <img id="logo" src="../../../assets/images/logo.png" alt="logo">
+                </a>
+            </div>
+
 
             <div class="navbar-element navbar-center-element" id="title">
                 Read The F****** Forum
@@ -23,6 +24,5 @@ class Navbar
             </div>
         </nav>
         <?php
-        (new \rtff\views\Layout('Navigation', ob_get_clean()))->show();
     }
 }
